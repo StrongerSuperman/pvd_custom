1fbdd9a3612d6329f3a7433f43a44016fb19b8e3
陈新宇 <chenxinyu01@corp.netease.com>
2021-04-07 12:56:08 +0800

    make the calculation of mouse picking with ray casting more clearly

--- src/logic/Camera.cpp
+++ src/logic/Camera.cpp
@@ -29,13 +29,13 @@ void Camera::HandleMouseLeftBtnDoubleClick(int x, int y)
 void Camera::updateRay(int x, int y)
 {
 	// code reference website: https://antongerdelan.net/opengl/raycasting.html
-	float mouseX = x / (GetViewPort().Width  * 0.5f) - 1.0f;
-	float mouseY = 1.0f - y / (GetViewPort().Height * 0.5f);
-	auto rayClip = glm::vec4(mouseX, mouseY, -1.0f, 1.0f);
-	auto rayEye = glm::inverse(GetProjectionMatrix()) * rayClip;
-	rayEye = glm::vec4(rayEye.x, rayEye.y, -1.0, 0.0);
-	auto rayDir = glm::vec3(glm::inverse(GetViewMatrix()) * rayEye);
-	rayDir = glm::normalize(rayDir);
+	float ndcX = 2.0 * x / GetViewPort().Width - 1.0f;
+	float ndcY = 1.0f - 2.0 * y / GetViewPort().Height;
+	auto rayClip = glm::vec4(ndcX, ndcY, -1.0f, 1.0f);
+	auto rayView = glm::inverse(GetProjectionMatrix()) * rayClip;
+	rayView = glm::vec4(rayView.x, rayView.y, -1.0, 0.0);
+	auto rayWorld = glm::vec3(glm::inverse(GetViewMatrix()) * rayView);
+	auto rayDir = glm::normalize(rayWorld);
 
 #ifndef COORDINATE_RIGHT_HANDED
 	m_Ray.SetOrigin(glm::vec3(GetEye().x, GetEye().y, -GetEye().z));
